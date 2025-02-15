a233f3a46f6e8dd91c394d48777f360ad5c20702
nimranaz148 <159953826+nimranaz148@users.noreply.github.com>
2025-02-15 22:36:07 +0500

    4

--- src/components/productGrid.tsx
+++ src/components/productGrid.tsx
@@ -24,6 +24,7 @@ export default function ProductsGrid() {
   //-----------------------------------------------Edit-Card-function
   const handleSaveProduct = async (updatedProduct: ICard) => {
     const res = await productPostSanity(updatedProduct)
+    
    if(res){
     setIsChange(!isChange)
    }
